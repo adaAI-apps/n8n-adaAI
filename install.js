314510e1182ea6267345e5fb00d5501b3518abaf
angga riyansyah <angga.adaai@gmail.com>
2025-05-01 19:33:02 +0700

    perubahan web UI

--- install.js
+++ install.js
@@ -2,17 +2,22 @@ module.exports = {
   run: [{
     method: "shell.run",
     params: {
-      message: "npm install -g n8n",
+      message: "conda install n8n -c conda-forge",
       path: "{{path}}/app",
       env: {
         N8N_PORT: "{{env.N8N_PORT || 5678}}"
       }
     }
   }, {
+    method: "fs.write",
+    params: {
+      path: "app/installed.txt",
+      text: "n8n installed successfully on {{new Date().toISOString()}}"
+    }
+  }, {
     method: "notify",
     params: {
-      html: "<b>Instalasi selesai!</b><br><br>Klik di sini untuk memulai n8n!",
-      href: "./start.js"
+      html: "<b>Instalasi selesai!</b><br><br>Klik 'Mulai' untuk menjalankan n8n!"
     }
   }]
 }
