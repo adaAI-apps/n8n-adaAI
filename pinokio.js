842f26f177dbacf2d923a25349ec29100ac205d1
angga riyansyah <angga.adaai@gmail.com>
2025-05-01 10:19:52 +0700

    perubahan pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -1,9 +1,5 @@
-const path = require("path");
 module.exports = {
   version: "2.0",
-  title: "n8n Workflow Automation",
-  description: "One-click installation for n8n, a fair-code workflow automation platform with 400+ integrations and AI capabilities",
-  icon: "icon.png",
   pre: [
     {
       icon: "docker.png",
@@ -11,82 +7,5 @@ module.exports = {
       description: "Docker is required to run n8n. Download and install Docker Desktop.",
       href: "https://www.docker.com/get-started"
     }
-  ],
-  menu: async (kernel, info) => {
-    let installed = info.exists("app/.installed");
-    let running = info.running("start.js");
-    if (installed && running) {
-      let memory = info.local("start.js");
-      return [
-        {
-          icon: "fa-solid fa-rocket",
-          text: "Web UI",
-          href: memory?.url || "http://localhost:{{env.N8N_PORT}}"
-        },
-        {
-          icon: "fa-solid fa-terminal",
-          text: "Terminal",
-          href: "start.js"
-        },
-        {
-          icon: "fa-solid fa-stop",
-          text: "Stop",
-          href: "stop.js"
-        },
-        {
-          icon: "fa-solid fa-rotate",
-          text: "Update",
-          href: "update.js"
-        },
-        {
-          icon: "fa-solid fa-broom",
-          text: "Factory Reset",
-          href: "reset.js"
-        },
-        {
-          icon: "fa-solid fa-plug",
-          text: "Reinstall",
-          href: "install.js"
-        }
-      ];
-    } else if (installed) {
-      return [
-        {
-          icon: "fa-solid fa-power-off",
-          text: "Start",
-          href: "start.js",
-          default: true
-        },
-        {
-          icon: "fa-solid fa-stop",
-          text: "Stop",
-          href: "stop.js"
-        },
-        {
-          icon: "fa-solid fa-rotate",
-          text: "Update",
-          href: "update.js"
-        },
-        {
-          icon: "fa-solid fa-broom",
-          text: "Factory Reset",
-          href: "reset.js"
-        },
-        {
-          icon: "fa-solid fa-plug",
-          text: "Reinstall",
-          href: "install.js"
-        }
-      ];
-    } else {
-      return [
-        {
-          icon: "fa-solid fa-plug",
-          text: "Install",
-          href: "install.js",
-          default: true
-        }
-      ];
-    }
-  }
+  ]
 };
